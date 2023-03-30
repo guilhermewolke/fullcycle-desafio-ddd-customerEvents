41641448f62635938e362f183f022dc4374034a3
Guilherme R. Woelke <guilherme_wolke@yahoo.com>
2023-03-30 15:12:27 -0300

    master
    - Implementação dos eventos a serem disparados no cadastro do cliente, e na alteração do endereço do cliente

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,3 +1,12 @@
+import Customer from "../../customer/entity/customer";
+import CustomerAddressChangedEvent from "../../customer/event/customer-address-changed.event";
+import CustomerCreatedEvent from "../../customer/event/customer-created.event";
+import EnviaConsoleLog1Handler from "../../customer/event/handler/envia-console-log1.handler";
+import SendEmailWhenCustomerChangesHisAddressHandler from "../../customer/event/handler/envia-console-log1.handler";
+import EnviaConsoleLogHandler from "../../customer/event/handler/envia-console.log.handler";
+import EnviaConsoleLog2Handler from "../../customer/event/handler/envia-console.log2.handler";
+import SendEmailWhenCustomerIsCreatedHandler from "../../customer/event/handler/envia-console.log2.handler";
+import Address from "../../customer/value-object/address";
 import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../../product/event/product-created.event";
 import EventDispatcher from "./event-dispatcher";
@@ -78,5 +87,72 @@ describe("Domain events tests", () => {
     eventDispatcher.notify(productCreatedEvent);
 
     expect(spyEventHandler).toHaveBeenCalled();
+  });
+
+  it("should register customer's creation event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const handler1 = new EnviaConsoleLog1Handler();
+    const handler2 = new EnviaConsoleLog2Handler();
+    const handler3 = new EnviaConsoleLogHandler();
+
+    eventDispatcher.register("CustomerCreatedEvent", handler1);
+    eventDispatcher.register("CustomerCreatedEvent", handler2);
+    eventDispatcher.register("CustomerAddressChangedEvent", handler3)
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+      2
+    );
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"].length).toBe(
+      1
+    );
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(handler1);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(handler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"][0]
+    ).toMatchObject(handler3);
+  });
+
+  it("should notify all customer's event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const handler1 = new EnviaConsoleLog1Handler();
+    const handler2 = new EnviaConsoleLog2Handler();
+    const handler3 = new EnviaConsoleLogHandler();
+    const spyEventHandler1 = jest.spyOn(handler1, "handle");
+    const spyEventHandler2 = jest.spyOn(handler2, "handle");
+    const spyEventHandler3 = jest.spyOn(handler3, "handle");
+    
+    eventDispatcher.register("CustomerCreatedEvent", handler1);
+    eventDispatcher.register("CustomerCreatedEvent", handler2);
+    eventDispatcher.register("CustomerAddressChangedEvent", handler3);
+
+    const customer = new Customer("1", "Caldrogo de Jesus");
+    const customerCreatedEvent = new CustomerCreatedEvent(customer);
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+
+    const address = new Address("Rua 1", 123, "11235-800", "Mogi das Cruzes");
+    customer.changeAddress(address);
+
+    const customerAddressChangedEvent = new CustomerAddressChangedEvent(customer);
+
+    eventDispatcher.notify(customerAddressChangedEvent);
+    expect(spyEventHandler3).toHaveBeenCalled();
   });
 });
